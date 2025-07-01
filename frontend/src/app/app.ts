dc1c096d0e5835b8b9d7a383dc8e4471511c0ab7
Thomas Schapakow <thomas.schapakow@fortis-it.de>
2025-07-01 12:38:37 +0200

    feat: add MongoDB support and refactor

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,18 +1,22 @@
 import {Component, OnInit} from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import {UserService} from './services/user';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App implements OnInit{
-  protected title = 'macher';
 
-  message: string = '';
-  constructor(private userService: UserService) {}
+  message: string = ''
+
+  constructor(
+    private userService: UserService
+  ) {
+
+  }
+
   ngOnInit() {
     this.userService.getUsers().subscribe(data => this.message = data);
   }
