3d4508212f2a8a6cc6e12525d81d4a33ede3040a
Thomas Schapakow <thomas.schapakow@fortis-it.de>
2025-07-02 09:43:48 +0200

    feat: add Todo component

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 import {UserService} from './services/user';
+import {Todo} from './todo/todo';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [
+    Todo,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
