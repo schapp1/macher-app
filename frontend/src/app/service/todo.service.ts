55a75f1eba48475fb913e0bb088bb9a235601023
Thomas Schapakow <thomas.schapakow@fortis-it.de>
2025-07-02 15:13:08 +0200

    refactor: clean up imports in todo service

--- frontend/src/app/service/todo.service.ts
+++ frontend/src/app/service/todo.service.ts
@@ -1,8 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {map, Observable} from 'rxjs';
+import {Observable} from 'rxjs';
 import {Todo} from '../models/todo';
-import {TodoCollection} from '../models/todo';
 
 @Injectable({
   providedIn: 'root'
